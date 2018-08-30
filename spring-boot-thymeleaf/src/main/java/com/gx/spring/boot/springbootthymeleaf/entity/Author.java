217e8ad41f0d9b78eb706a9d2052c25815b20f14
gaoxu <457562774@qq.com>
2018-08-30 15:23:31 +0800

    Thymeleaf模板

---
+++ spring-boot-thymeleaf/src/main/java/com/gx/spring/boot/springbootthymeleaf/entity/Author.java
@@ -0,0 +1,35 @@
+package com.gx.spring.boot.springbootthymeleaf.entity;
+
+/**
+ * @author: gaoxu
+ * @date: 2018/8/30
+ */
+public class Author {
+    private int age;
+    private String name;
+    private String email;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
