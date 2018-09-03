cedc1f5a39defce82a27b16642fb505e4ecc58c9
gaoxu <457562774@qq.com>
2018-09-03 14:00:19 +0800

    SpringDataJpa

---
+++ spring-boot-jpa/src/main/java/com/gx/springbootjpa/entity/User.java
@@ -0,0 +1,71 @@
+package com.gx.springbootjpa.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity(name = "t_user")
+public class User implements Serializable {
+    //主键自增
+//    TABLE： 使用一个特定的数据库表格来保存主键
+//    SEQUENCE： 根据底层数据库的序列来生成主键，条件是数据库支持序列。这个值要与generator一起使用，generator 指定生成主键使用的生成器（可能是orcale中自己编写的序列）。
+//    IDENTITY： 主键由数据库自动生成（主要是支持自动增长的数据库，如mysql）
+//    AUTO： 主键由程序控制，也是GenerationType的默认值。
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    //用户名
+    private String username;
+    //密码
+    private String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+}
