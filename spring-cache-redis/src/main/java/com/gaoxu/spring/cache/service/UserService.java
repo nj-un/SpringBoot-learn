621b684fc46fe1db3b74fef25f37136a736907ed
gaoxu <457562774@qq.com>
2018-09-04 09:49:22 +0800

    实现类

---
+++ spring-cache-redis/src/main/java/com/gaoxu/spring/cache/service/UserService.java
@@ -0,0 +1,33 @@
+package com.gaoxu.spring.cache.service;
+
+import com.gaoxu.spring.cache.entity.User;
+
+/**
+ * @author: gaoxu
+ * @date: 2018/9/4
+ */
+public interface UserService {
+
+    /**
+     * 删除
+     *
+     * @param user 用户对象
+     * @return 操作结果
+     */
+    User saveOrUpdate(User user);
+
+    /**
+     * 添加
+     *
+     * @param id key值
+     * @return 返回结果
+     */
+    User get(Long id);
+
+    /**
+     * 删除
+     *
+     * @param id key值
+     */
+    void delete(Long id);
+}
