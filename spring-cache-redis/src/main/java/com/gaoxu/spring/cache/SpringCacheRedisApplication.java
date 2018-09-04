f063fc000e1e7a9306a6705e36f9bdb2ddffc0e0
gaoxu <457562774@qq.com>
2018-09-04 09:32:16 +0800

    Spring Cache集成Redis

---
+++ spring-cache-redis/src/main/java/com/gaoxu/spring/cache/SpringCacheRedisApplication.java
@@ -0,0 +1,12 @@
+package com.gaoxu.spring.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCacheRedisApplication.class, args);
+    }
+}
