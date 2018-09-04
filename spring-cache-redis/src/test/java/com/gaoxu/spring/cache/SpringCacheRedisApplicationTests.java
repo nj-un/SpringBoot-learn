2ab1a411c9b4d9d722acfdb0eca1f8945ecd3f2a
gaoxu <457562774@qq.com>
2018-09-04 09:49:50 +0800

    Spring Cache集成Redis

--- spring-cache-redis/src/test/java/com/gaoxu/spring/cache/SpringCacheRedisApplicationTests.java
+++ spring-cache-redis/src/test/java/com/gaoxu/spring/cache/SpringCacheRedisApplicationTests.java
@@ -1,7 +1,12 @@
 package com.gaoxu.spring.cache;
 
+import com.gaoxu.spring.cache.entity.User;
+import com.gaoxu.spring.cache.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringCacheRedisApplicationTests {
 
+    private static final Logger log = LoggerFactory.getLogger(SpringCacheRedisApplicationTests.class);
+
+    @Autowired
+    private UserService userService;
+
     @Test
     public void contextLoads() {
+        final User user = userService.saveOrUpdate(new User(5L, "u5", "p5"));
+        log.info("[saveOrUpdate] - [{}]", user);
+        final User user1 = userService.get(5L);
+        log.info("[get] - [{}]", user1);
+        userService.delete(5L);
     }
 
 }
