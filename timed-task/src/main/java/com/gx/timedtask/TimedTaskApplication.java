f120c3f4eb9b1fe4b5a73eaf04bb6f24bb7d86d3
gaoxu <457562774@qq.com>
2018-09-07 17:34:35 +0800

    定时任务详解

---
+++ timed-task/src/main/java/com/gx/timedtask/TimedTaskApplication.java
@@ -0,0 +1,12 @@
+package com.gx.timedtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimedTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimedTaskApplication.class, args);
+    }
+}
---
+++ timed-task/src/main/java/com/gx/timedtask/timer/TimerDemo.java
@@ -0,0 +1,27 @@
+package com.gx.timedtask.timer;
+
+import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * 基于Timer实现的定时调度（不推荐，用该方式不如用 ScheduledExecutorService ）
+ *
+ * @author: gaoxu
+ * @date: 2018/9/7
+ */
+public class TimerDemo {
+    public static void main(String[] args) {
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("执行任务:" + LocalDateTime.now());
+            }
+        };
+        Timer timer = new Timer();
+        // timerTask：需要执行的任务
+        // delay：延迟时间（以毫秒为单位）
+        // period：间隔时间（以毫秒为单位）
+        timer.schedule(timerTask, 5000, 3000);
+    }
+}
