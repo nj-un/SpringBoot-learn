908534b99f5aed80f009660f0434aea2dafa226c
gaoxu <457562774@qq.com>
2018-09-18 14:52:41 +0800

    数据验证

---
+++ validate/src/main/java/com/gx/validate/annotation/DateTime.java
@@ -0,0 +1,31 @@
+package com.gx.validate.annotation;
+
+import com.gx.validate.validator.DateTimeValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * @author: gaoxu
+ * @date: 2018/9/18
+ */
+@Target({FIELD, PARAMETER})
+@Retention(RUNTIME)
+@Constraint(validatedBy = DateTimeValidator.class)
+public @interface DateTime {
+
+    String message() default "格式错误";
+
+    String format() default "yyyy-MM-dd";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
