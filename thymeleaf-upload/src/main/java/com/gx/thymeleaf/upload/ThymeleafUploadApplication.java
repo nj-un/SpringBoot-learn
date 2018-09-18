3b39b58ff714d6e4a4e70fdb2c867e443a7133c7
gaoxu <457562774@qq.com>
2018-09-18 11:12:17 +0800

    Thymeleaf 上传文件

---
+++ thymeleaf-upload/src/main/java/com/gx/thymeleaf/upload/ThymeleafUploadApplication.java
@@ -0,0 +1,12 @@
+package com.gx.thymeleaf.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafUploadApplication.class, args);
+    }
+}
