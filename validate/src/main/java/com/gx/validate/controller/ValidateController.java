686921514026f79c89fc468148c4114ee996d803
gaoxu <457562774@qq.com>
2018-09-18 15:01:41 +0800

    数据验证

--- validate/src/main/java/com/gx/validate/controller/ValidateController.java
+++ validate/src/main/java/com/gx/validate/controller/ValidateController.java
@@ -2,6 +2,8 @@ package com.gx.validate.controller;
 
 import com.gx.validate.annotation.DateTime;
 import com.gx.validate.entity.Book;
+import com.gx.validate.entity.Greens;
+import com.gx.validate.groups.Groups;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,6 +32,16 @@ public class ValidateController {
     @GetMapping("/test3")
     public String test3(@Validated Book book) {
         return "success";
+    }
+
+    @GetMapping("/insert")
+    public String insert(@Validated(value = Groups.Default.class) Greens greens) {
+        return "insert";
+    }
+
+    @GetMapping("/update")
+    public String update(@Validated(value = {Groups.Default.class, Groups.Update.class}) Greens greens) {
+        return "update";
     }
 
 }
---
+++ validate/src/main/java/com/gx/validate/groups/Groups.java
@@ -0,0 +1,16 @@
+package com.gx.validate.groups;
+
+/**
+ * @author: gaoxu
+ * @date: 2018/9/18
+ */
+public class Groups {
+
+    public interface Update {
+
+    }
+
+    public interface Default {
+
+    }
+}
