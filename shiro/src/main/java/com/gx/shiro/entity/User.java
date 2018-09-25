0d71fb2b754ea0aabf7a431cb5126e64338c827f
gaoxu <457562774@qq.com>
2018-09-25 17:12:47 +0800

    安全框架（Shiro）

---
+++ shiro/src/main/java/com/gx/shiro/entity/User.java
@@ -0,0 +1,79 @@
+package com.gx.shiro.entity;
+
+public class User {
+    //主键自增
+    private Long id;
+    //用户名
+    private String username;
+    //密码
+    private String password;
+    /** 角色名：Shiro 支持多个角色，而且接收参数也是 Set<String> 集合，但这里为了简单起见定义成 String 类型了 */
+    private String roleName;
+
+    /** 是否禁用 */
+    private boolean locked;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+}
